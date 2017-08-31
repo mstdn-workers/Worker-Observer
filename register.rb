faf8e1d2e4f18aa4317959bd91ccb7e4629c8453
sa2taka <sa2taka@gmail.com>
2017-09-01 02:35:05 +0900

    追加: Registerモジュールの追加と、それに伴い定数の場所の変更

--- register.rb
+++ register.rb
@@ -8,10 +8,20 @@ require 'readline'
 
 require 'json'
 
-APP_NAME = "Name Change Detecter"
-DEFAULT_CONFIG = {
-  "host" => "mstdn-workers.com",
-  "scopes" => "read write",
-}
-TOKEN_FILE_NAME = '.access_token'
-DEFAULT_CONFIG_FILE_NAME = 'config.json'
+module Register
+  attr_reader :config
+
+  APP_NAME = "Name Change Detecter"
+  DEFAULT_CONFIG = {
+    "host" => "mstdn-workers.com",
+    "scopes" => "read write",
+  }
+  TOKEN_FILE_NAME = '.access_token'
+  DEFAULT_CONFIG_FILE_NAME = 'config.json'
+
+  def init_app
+    base_url = 'https://' + @config["host"]
+    Mastodon::REST::Client.new(base_url: base_url,
+                               bearer_token: access_token)
+  end
+end
