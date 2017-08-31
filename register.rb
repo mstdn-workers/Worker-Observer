f9a2b42a06752ff1f1c5455d3c9bcd93731fa990
sa2taka <sa2taka@gmail.com>
2017-09-01 02:42:04 +0900

    追加: appの初期化メソッドの追加

--- register.rb
+++ register.rb
@@ -6,11 +6,10 @@ require 'bundler/setup'
 require 'mastodon'
 require 'readline'
 
+require "oauth2"
 require 'json'
 
 module Register
-  attr_reader :config
-
   APP_NAME = "Name Change Detecter"
   DEFAULT_CONFIG = {
     "host" => "mstdn-workers.com",
@@ -20,8 +19,24 @@ module Register
   DEFAULT_CONFIG_FILE_NAME = 'config.json'
 
   def init_app
-    base_url = 'https://' + @config["host"]
+    base_url = 'https://' + config["host"]
     Mastodon::REST::Client.new(base_url: base_url,
                                bearer_token: access_token)
   end
+
+  def config
+    if File.exist? DEFAULT_CONFIG_FILE_NAME
+      JSON.parse(File.read(DEFAULT_CONFIG_FILE_NAME))
+    else
+      save_config(DEFAULT_CONFIG)
+      DEFAULT_CONFIG
+    end
+  end
+
+  def save_config(config)
+    File.write(DEFAULT_CONFIG_FILE_NAME, JSON.dump(config))
+  end
 end
+
+include Register
+p init_app
