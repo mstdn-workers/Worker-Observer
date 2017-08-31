63317f5935169e05c1025897cee001a26f2b8bec
sa2taka <sa2taka@gmail.com>
2017-09-01 03:24:27 +0900

    追加: LTLの読み込み機能の追加

--- simple_mastodon.rb
+++ simple_mastodon.rb
@@ -4,6 +4,35 @@ class SimpleMastodon
   include Register
 
   def initialize
-    
+    @client = init_app
+    @since_id = 0
+  end
+
+  def local_time_line
+    ret_val = []
+    @client.public_timeline(since_id: @since_id, local: true).each do |status|
+      ret_val << status_to_string(status)
+    end
+    ret_val.reverse
+  end
+
+  private
+
+  def status_to_string(status)
+    account = status.account
+    content = status.content
+    display_name = account.display
+    display_name ||= account.acct
+    [display_name, "@" + account.acct, content_convert(content)]
+  end
+
+  def content_convert(content)
+    require 'cgi'
+    content.gsub!("<br \/>", "\n")
+    remove_tag(content)
+  end
+
+  def remove_tag(str)
+    str.gsub(/<([^>]+)>/, "")
   end
 end
