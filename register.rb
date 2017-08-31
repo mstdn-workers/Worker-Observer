1f3d4cf73110cfccb51d4ded4d3d44fed03b246e
sa2taka <sa2taka@gmail.com>
2017-09-01 02:16:22 +0900

    追加: 登録時に必要な定数の追加

--- register.rb
+++ register.rb
@@ -7,3 +7,11 @@ require 'mastodon'
 require 'readline'
 
 require 'json'
+
+APP_NAME = "Name Change Detecter"
+DEFAULT_CONFIG = {
+  "host" => "mstdn-workers.com",
+  "scopes" => "read write",
+}
+TOKEN_FILE_NAME = '.access_token'
+DEFAULT_CONFIG_FILE_NAME = 'config.json'
