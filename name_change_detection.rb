d513c7d6f541495f43287d9faaba5b32fa1caa70
sa2taka <sa2taka@gmail.com>
2017-09-03 15:36:48 +0900

    追加: Mainクラスにinitializeメソッドとそれに追随するメソッドを追加

--- name_change_detection.rb
+++ name_change_detection.rb
@@ -3,6 +3,29 @@ require './database'
 module NameChangeDetection
   # 今回のメイン機能を実装したクラス
   class Main
+    def initialize
+      @manager = SimpleMastodon.new
+    end
 
+    def register_thread
+      ncd_thread = Thread.new do
+        sleep(5.0)
+        name_change_detection
+      end
+      ncd_thread.join
+
+      rm_thread = Thread.new do
+        sleep(2.0)
+      end
+      rm_thread.join
+    end
+
+    def name_change_detection
+
+    end
+
+    def reaction_mention
+
+    end
   end
 end
