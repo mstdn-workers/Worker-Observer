442f7bca0ce800b2603425152050cafa4428a2ef
sa2taka <sa2taka@gmail.com>
2017-09-03 14:03:03 +0900

    追加: Namesを編集するためのメソッドの追加

--- name_change_detection.rb
+++ name_change_detection.rb
@@ -11,4 +11,17 @@ class NameChangeDetection
     account = Accounts.find_by(id: id)
     account.update(nickname: nickname) unless account.nil?
   end
+
+  def register_name(account_id, display)
+    Names.find_or_create_by(account_id: account_id, display_name: display) do |n|
+      time = Time.now.strftime("%Y/%m/%d %H:%M:%S")
+      n.changed_date = time
+      n.is_first = exist?(account_id) ? 0 : 1
+    end
+    p Names.all
+  end
+
+  def exist?(id)
+    !Names.find_by(account_id: id).nil?
+  end
 end
