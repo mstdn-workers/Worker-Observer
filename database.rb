7af854e7af3afdd52d250479c07550ffb79a3ea6
sa2taka <sa2taka@gmail.com>
2017-09-03 14:50:50 +0900

    変更: NameChangeDetectionモジュール内のDatabaseクラスにDatabaseのラッピングメソッドに追加

--- database.rb
+++ database.rb
@@ -13,3 +13,53 @@ end
 
 class Names < ActiveRecord::Base
 end
+
+# データベース接続クラス
+module NameChangeDetection
+  class Database
+    # Accountテーブルに新たな要素を作るメソッド。作成時にはnicknameが登録されることはない
+    def register_account(id, username)
+      Accounts.find_or_create_by(id: id) { |a| a.username = username }
+    end
+
+    def set_nickname(id, nickname)
+      account = Accounts.find_by(id: id)
+      account.update(nickname: nickname) unless account.nil?
+    end
+
+    # @param element[:id] = id
+    # @param element[:username] = acct
+    def accounts(element = nil)
+      if element.nil?
+        Accounts.all
+      elsif element[:id]
+        Accounts.find_by(id: element[:id])
+      elsif element[:username]
+        Accounts.find_by(username: element[:username])
+      else
+        Accounts.all
+      end
+    end
+
+    def register_name(account_id, display)
+      Names.find_or_create_by(account_id: account_id, display_name: display) do |n|
+        time = Time.now.strftime("%Y/%m/%d %H:%M:%S")
+        n.changed_date = time
+        n.is_first = exist?(account_id) ? 0 : 1
+      end
+      p Names.all
+    end
+
+    def names(id = nil)
+      if id
+        Names.find_by(account_id: id)
+      else
+        Names.all
+      end
+    end
+
+    def exist?(id)
+      !Names.find_by(account_id: id).nil?
+    end
+  end
+end
