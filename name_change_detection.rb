1e730f486b91b9267cdfc1b4dd1edf33edb0f967
sa2taka <sa2taka@gmail.com>
2017-09-04 12:29:19 +0900

    追加: debug機構といくつかのdebugコマンドを追加

--- name_change_detection.rb
+++ name_change_detection.rb
@@ -15,10 +15,10 @@ module NameChangeDetection
 
     # threadを追加するメソッド
     def register_thread
-      @detection_thread = create_thread(:name_change_detection, 5)
-      @detection_thread.join
-      @reaction_thread = create_thread(:reaction_mention, 2)
-      @reaction_thread.join
+      #@detection_thread = create_thread(:name_change_detection, 5)
+      #@detection_thread.join
+      #@reaction_thread = create_thread(:reaction_mention, 2)
+      #@reaction_thread.join
       @debug_thread = create_thread(:debug, 0)
       @debug_thread.join
     end
@@ -33,8 +33,8 @@ module NameChangeDetection
     end
 
     def stop
-      @detection_thread.kill
-      @reaction_thread.kill
+      #@detection_thread.kill
+      #@reaction_thread.kill
       @debug_thread.kill
     end
 
@@ -52,6 +52,37 @@ module NameChangeDetection
     end
 
     def debug
+      # debug用のメソッドは"d_"というprefixをつけるため
+      command = ("d_" + gets.chomp).to_sym
+      respond_to?(command) ? send(command) : d_help
+    end
+
+    # debug用コマンド
+
+    def d_help
+      puts "デバッグ時のコマンド一覧"
+      methods.each do |method|
+        puts method.to_s.delete("d_") if method.to_s.start_with?("d_")
+      end
+      puts
+    end
+
+    def d_start
+      stop
+      start
+    end
+
+    def d_stop
+      stop_without_debug
+    end
+
+    def d_all_stop
+      stop
+    end
+
+    def stop_without_debug
+      #@detection_thread.kill
+      #@reaction_thread.kill
     end
   end
 end
