cc1b79ee2debb6a167beed35d8f580dbd6f0edc2
sa2taka <sa2taka@gmail.com>
2017-10-18 23:08:07 +0900

    追加: indexの追記と、テストvueスクリプトを追加

---
+++ public/script/app.js
@@ -0,0 +1,11 @@
+const names = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {username: "hoge", display: "fuga", changed: "2012/08/02 20:12:34"},
+      {username: "test", display: "fuga", changed: "2012/08/02 20:12:36"},
+      {username: "test2", display: "fuga", changed: "2012/08/02 20:12:38"},
+      {username: "test3", display: "fuga", changed: "2012/08/02 20:12:40"}
+    ]
+  }
+});
