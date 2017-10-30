b94524d09343044e97b2166dd50248331f0e835d
sa2taka <sa2taka@gmail.com>
2017-10-30 23:04:04 +0900

    修正: データベースクラスの名前を変更

--- database.rb
+++ database.rb
@@ -12,11 +12,11 @@ Time.zone = 'Tokyo'
 Time.zone_default = Time.find_zone! 'Tokyo'
 ActiveRecord::Base.default_timezone = :local
 
-class Accounts < ActiveRecord::Base
+class Account < ActiveRecord::Base
   has_many :names
 end
 
-class Names < ActiveRecord::Base
+class Name < ActiveRecord::Base
   belongs_to :account
 end
 
@@ -26,11 +26,11 @@ module NameChangeDetection
     include Singleton
     # Accountテーブルに新たな要素を作るメソッド。作成時にはnicknameが登録されることはない
     def register_account(id, username)
-      Accounts.find_or_create_by(id: id) { |a| a.username = username }
+      Account.find_or_create_by(id: id) { |a| a.username = username }
     end
 
     def set_nickname(id, nickname)
-      account = Accounts.find_by(id: id)
+      account = Account.find_by(id: id)
       account.update(nickname: nickname) unless account.nil?
     end
 
@@ -38,13 +38,13 @@ module NameChangeDetection
     # @param element[:username] = acct
     def accounts(element = nil)
       if element.nil?
-        Accounts.order("id DESC")
+        Account.order("id DESC")
       elsif element[:id]
-        Accounts.where(id: element[:id])
+        Account.where(id: element[:id])
       elsif element[:username]
-        Accounts.where(username: element[:username])
+        Account.where(username: element[:username])
       else
-        Accounts.all
+        Account.all
       end
     end
 
@@ -54,7 +54,7 @@ module NameChangeDetection
       return if newest && display == newest.display_name
       puts "new_name: #{display}"
 
-      name = Names.new do |n|
+      name = Name.new do |n|
         n.account_id = account_id
         n.display_name = display
         n.is_first = exist?(account_id) ? 0 : 1
@@ -68,20 +68,20 @@ module NameChangeDetection
     # @param element[:username] = acct
     def names(element = nil)
       if element.nil?
-        Names.joins(:account).order("id DESC").all
+        Name.joins(:account).order("id DESC").all
       elsif element[:id]
-        Names.joins(:account).order("id DESC").where(account_id: element[:id])
+        Name.joins(:account).order("id DESC").where(account_id: element[:id])
       elsif element[:username]
-        Names.joins(:account).order("id DESC").where(username: element[:username])
+        Name.joins(:account).order("id DESC").where(username: element[:username])
       else
-        Names.joins(:account).order("id DESC").all
+        Name.joins(:account).order("id DESC").all
       end
     end
 
     private
 
     def exist?(id)
-      !Names.find_by(account_id: id).nil?
+      !Name.find_by(account_id: id).nil?
     end
   end
 end
